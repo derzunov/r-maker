4c2c94c049b99097f26584e99e04194133a7056f
derzunov <derzunov.work@yandex.ru>
2026-04-17 17:20:51 +0300

    init commits and readme.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log( 'R-maker' );
+
+// see https://github.com/apurvaojas/docx-merger#readme
+
+// var DocxMerger = require('docx-merger');
+//
+// var fs = require('fs');
+// var path = require('path');
+//
+// var file1 = fs
+//     .readFileSync(path.resolve(__dirname, 'template.docx'), 'binary');
+//
+// var file2 = fs
+//     .readFileSync(path.resolve(__dirname, 'template1.docx'), 'binary');
+//
+// var docx = new DocxMerger({},[file1,file2]);
+//
+//
+// //SAVING THE DOCX FILE
+//
+// docx.save('nodebuffer',function (data) {
+//     // fs.writeFile("output.zip", data, function(err){/*...*/});
+//     fs.writeFile("output.docx", data, function(err){/*...*/});
+// });
